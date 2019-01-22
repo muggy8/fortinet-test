cd0ac5e2362f64cb228c061a0f97c72bed20185d
muggy8 <muggy8@gmail.com>
2019-01-22 13:02:03 -0800

    list now also displays the original names

--- api/list.php
+++ api/list.php
@@ -3,6 +3,7 @@ require_once("utils.php");
 isGet(true);
 
 $path = CONTENT_FOLDER . $_GET["path"];
+$pathMetaDataFile = $path . METADATA_FILE;
 
 if (!file_exists($path)){
 	http_response_code(404);
@@ -10,6 +11,13 @@ if (!file_exists($path)){
 		"message" => "Directory Not Found"
 	]);
 	exit();
+}
+
+if (file_exists($pathMetaDataFile)){
+	$pathMetaData = json_decode(file_get_contents($pathMetaDataFile), true);
+}
+else{
+	$pathMetaData = [];
 }
 
 // get all the contents
@@ -26,9 +34,12 @@ $contents = array_filter($contents, function($relPath){
 $contents = array_values($contents);
 
 // now we figure out if each file is a file or a folder
-$contents = array_map(function($relPath) use ($path) {
+$contents = array_map(function($relPath) use ($path, $pathMetaData) {
 	return [
 		"name" => $relPath,
+		"srcName" => array_key_exists($relPath, $pathMetaData)
+			? $pathMetaData[$relPath]
+			: "",
 		"type" => is_dir($path . $relPath) ? "dir" : "file"
 	];
 }, $contents);
