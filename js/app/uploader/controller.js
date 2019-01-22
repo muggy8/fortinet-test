c24ff1fbb1990e9b1d53d91d2ea3c6732d964bd9
muggy8 <muggy8@gmail.com>
2019-01-22 06:49:01 -0800

    the file uploader is now able to add files to the internal upload list

--- js/app/uploader/controller.js
+++ js/app/uploader/controller.js
@@ -1,5 +1,46 @@
 "use strict"
 angular.module("uploader-app")
-	.controller("uploader-controller", ["$scope", "$dialogue", "api", function($scope, $dialogue, api){
+	.controller("uploader-controller", ["$scope", "$timeout", "$dialogue", "api", function($scope, $timeout, $dialogue, api){
 		let scope = $scope
+
+		$timeout(function(){
+			// do this on the next tick once the view has been appended
+			document.getElementById("file-select").scope = scope
+		})
+
+		scope.files = []
+
+		function addFiles(files){
+			Array.prototype.forEach.call(files, function(file){
+				scope.files.push({
+					progres: 0,
+					file: file,
+					name: file.name
+				})
+			})
+		}
+
+		scope.externals = {
+			fileDrop: function(event){
+				event.preventDefault()
+				event.stopPropagation()
+
+				let files = event.dataTransfer.files
+
+				addFiles(files)
+
+				scope.$apply()
+			},
+			selectChange: function(event, self){
+				event.preventDefault()
+				event.stopPropagation()
+
+				let files = self.files
+
+				addFiles(files)
+
+				scope.$apply()
+			}
+		}
+
 	}])
