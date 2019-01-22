6b07509dab6532248c4db4fe657b95b6d791a0ec
muggy8 <muggy8@gmail.com>
2019-01-22 09:32:17 -0800

    echo out the stuff for debug

--- api/upload.php
+++ api/upload.php
@@ -1,1 +1,8 @@
 <?php
+require_once("utils.php");
+require_once("../deps/class.uploader.php");
+isPost(true);
+
+$uploaded = file_get_contents("php://input");
+var_dump($uploaded);
+var_dump($_FILES);
